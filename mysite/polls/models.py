3cd453583703ce6ad71e8111f5c51780ea57fe68
anny-panny <anny_pan@live.cn>
2016-09-29 16:53:20 -0600

    lab4

---
+++ mysite/polls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible
+class Question(models.Model): #this is going to be a table
+	#this is going to be column/field in the Question table
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.question_text
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+	question = models.ForeignKey(Question,on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return self.choice_text
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/cshome/xpan/c404/l4/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
